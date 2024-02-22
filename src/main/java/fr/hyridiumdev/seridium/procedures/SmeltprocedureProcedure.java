e5b15486f299ff0b71cf0b04b4f9c9d70c98a628
Nalaxyzz <Nalaxyzz@DESKTOP-668JVRU.home>
2024-02-22 15:58:57 +0100

    V0.0.4 NEW SMELTING

--- src/main/java/fr/hyridiumdev/seridium/procedures/SmeltprocedureProcedure.java
+++ src/main/java/fr/hyridiumdev/seridium/procedures/SmeltprocedureProcedure.java
@@ -1,9 +1,5 @@
 package fr.hyridiumdev.seridium.procedures;
 
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.event.world.BlockEvent;
-
 import net.minecraft.world.World;
 import net.minecraft.world.IWorld;
 import net.minecraft.util.math.BlockPos;
@@ -11,40 +7,15 @@ import net.minecraft.item.crafting.IRecipeType;
 import net.minecraft.item.ItemStack;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.entity.item.ItemEntity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.Entity;
-import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.Block;
 
 import java.util.Map;
-import java.util.HashMap;
 
-import fr.hyridiumdev.seridium.enchantment.SmeltEnchantment;
+import fr.hyridiumdev.seridium.item.SmeltPickaxeSeridiumItem;
 import fr.hyridiumdev.seridium.SeridiumMod;
 
 public class SmeltprocedureProcedure {
-	@Mod.EventBusSubscriber
-	private static class GlobalTrigger {
-		@SubscribeEvent
-		public static void onBlockBreak(BlockEvent.BreakEvent event) {
-			Entity entity = event.getPlayer();
-			IWorld world = event.getWorld();
-			Map<String, Object> dependencies = new HashMap<>();
-			dependencies.put("xpAmount", event.getExpToDrop());
-			dependencies.put("x", event.getPos().getX());
-			dependencies.put("y", event.getPos().getY());
-			dependencies.put("z", event.getPos().getZ());
-			dependencies.put("px", entity.getPosX());
-			dependencies.put("py", entity.getPosY());
-			dependencies.put("pz", entity.getPosZ());
-			dependencies.put("world", world);
-			dependencies.put("entity", entity);
-			dependencies.put("blockstate", event.getState());
-			dependencies.put("event", event);
-			executeProcedure(dependencies);
-		}
-	}
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
 		if (dependencies.get("world") == null) {
@@ -67,49 +38,47 @@ public class SmeltprocedureProcedure {
 				SeridiumMod.LOGGER.warn("Failed to load dependency z for procedure Smeltprocedure!");
 			return;
 		}
-		if (dependencies.get("entity") == null) {
-			if (!dependencies.containsKey("entity"))
-				SeridiumMod.LOGGER.warn("Failed to load dependency entity for procedure Smeltprocedure!");
-			return;
-		}
 		IWorld world = (IWorld) dependencies.get("world");
 		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
 		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
 		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
-		Entity entity = (Entity) dependencies.get("entity");
-		if (EnchantmentHelper.getEnchantmentLevel(SmeltEnchantment.enchantment,
-				((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY)) > 0) {
-			if (((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY).getItem()
-					.canHarvestBlock((world.getBlockState(new BlockPos(x, y, z)))) == true) {
-				if (((world instanceof World)
-						? ((World) world).getRecipeManager()
-								.getRecipe(IRecipeType.SMELTING,
-										new Inventory((new ItemStack((world.getBlockState(new BlockPos(x, y, z))).getBlock()))), (World) world)
-								.isPresent()
-						: false) == true) {
-					if (world instanceof World && !world.isRemote()) {
-						ItemEntity entityToSpawn = new ItemEntity((World) world, (x + 0.5), (y + 0.5), (z + 0.5),
-								((world instanceof World && ((World) world).getRecipeManager()
-										.getRecipe(IRecipeType.SMELTING,
-												new Inventory((new ItemStack((world.getBlockState(new BlockPos(x, y, z))).getBlock()))),
-												((World) world))
-										.isPresent())
-												? ((World) world).getRecipeManager()
-														.getRecipe(IRecipeType.SMELTING,
-																new Inventory(
-																		(new ItemStack((world.getBlockState(new BlockPos(x, y, z))).getBlock()))),
-																(World) world)
-														.get().getRecipeOutput().copy()
-												: ItemStack.EMPTY));
-						entityToSpawn.setPickupDelay((int) 10);
-						world.addEntity(entityToSpawn);
-					}
-					world.setBlockState(new BlockPos(x, y, z), Blocks.AIR.getDefaultState(), 3);
-				} else {
-					if (world instanceof World) {
-						Block.spawnDrops(world.getBlockState(new BlockPos(x, y, z)), (World) world, new BlockPos(x, y, z));
-						world.destroyBlock(new BlockPos(x, y, z), false);
-					}
+		boolean removeBlock = false;
+		if (SmeltPickaxeSeridiumItem.block.canHarvestBlock((world.getBlockState(new BlockPos(x, y, z)))) == true) {
+			if (!(((world instanceof World && ((World) world).getRecipeManager()
+					.getRecipe(IRecipeType.SMELTING, new Inventory((new ItemStack((world.getBlockState(new BlockPos(x, y, z))).getBlock()))),
+							((World) world))
+					.isPresent())
+							? ((World) world).getRecipeManager()
+									.getRecipe(IRecipeType.SMELTING,
+											new Inventory((new ItemStack((world.getBlockState(new BlockPos(x, y, z))).getBlock()))), (World) world)
+									.get().getRecipeOutput().copy()
+							: ItemStack.EMPTY)
+					.getItem() == Blocks.AIR.asItem())) {
+				if (world instanceof World && !world.isRemote()) {
+					ItemEntity entityToSpawn = new ItemEntity((World) world, (x + 0.5), (y + 0.5), (z + 0.5),
+							((world instanceof World && ((World) world).getRecipeManager()
+									.getRecipe(IRecipeType.SMELTING,
+											new Inventory((new ItemStack((world.getBlockState(new BlockPos(x, y, z))).getBlock()))), ((World) world))
+									.isPresent())
+											? ((World) world).getRecipeManager()
+													.getRecipe(IRecipeType.SMELTING,
+															new Inventory((new ItemStack((world.getBlockState(new BlockPos(x, y, z))).getBlock()))),
+															(World) world)
+													.get().getRecipeOutput().copy()
+											: ItemStack.EMPTY));
+					entityToSpawn.setPickupDelay((int) 0);
+					world.addEntity(entityToSpawn);
+				}
+				removeBlock = (true);
+			} else {
+				removeBlock = (false);
+			}
+			if (removeBlock == true) {
+				world.destroyBlock(new BlockPos(x, y, z), false);
+			} else {
+				if (world instanceof World) {
+					Block.spawnDrops(world.getBlockState(new BlockPos(x, y, z)), (World) world, new BlockPos(x, y, z));
+					world.destroyBlock(new BlockPos(x, y, z), false);
 				}
 			}
 		}
